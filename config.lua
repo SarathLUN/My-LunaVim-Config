dbcaf7ca86a98fef4ff285826d0737f85b60e789
SarathLUN <lun.sarath@gmail.com>
2024-06-14 22:13:53 +0700

    first commit for my LunaVim config with rainbow_csv

---
+++ config.lua
@@ -0,0 +1,34 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Configure rust-analyzer.cargo.sysroot in LunaVim:
+require'lspconfig'.rust_analyzer.setup{
+    settings = {
+        ["rust-analyzer.cargo.sysroot"] = "/Users/sarath.lun/.rustup/toolchains/stable-aarch64-apple-darwin"
+    }
+}
+
+lvim.plugins = {
+    {'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+        'csv',
+        'tsv',
+        'csv_semicolon',
+        'csv_whitespace',
+        'csv_pipe',
+        'rfc_csv',
+        'rfc_semicolon'
+    },
+    cmd = {
+        'RainbowDelim',
+        'RainbowDelimSimple',
+        'RainbowDelimQuoted',
+        'RainbowMultiDelim'
+    },
+  },
+}
+
