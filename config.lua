9117c9992a3b9dfb4f2fae5b808012eb9fd40e46
SarathLUN <lun.sarath@gmail.com>
2025-02-01 14:25:41 +0700

    add keymap for bufferlinenext

--- config.lua
+++ config.lua
@@ -3,59 +3,106 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-
 -- general option:
 vim.opt.relativenumber = true -- relative line numbers
+vim.opt.mouse = "" -- disable mouse click
 
+-- My custom keymaps:
+-- Shift+h switches to the previous tab
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+
+-- end of my custom keymaps
+
+-- setup and customize colorscheme for tokyonight-night:
+require("tokyonight").setup({
+	-- your configuration comes here
+	-- or leave it empty to use the default settings
+	style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+	transparent = false, -- Enable this to disable setting the background color
+	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+	styles = {
+		-- Style to be applied to different syntax groups
+		-- Value is any valid attr-list value for `:help nvim_set_hl`
+		comments = { italic = true },
+		keywords = { italic = true },
+		functions = {},
+		variables = {},
+		-- Background styles. Can be "dark", "transparent" or "normal"
+		sidebars = "dark", -- style for sidebars, see below
+		floats = "dark", -- style for floating windows
+	},
+	sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+	day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+	hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+	dim_inactive = false, -- dims inactive windows
+	lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+	--- You can override specific color groups to use other groups or a hex color
+	--- function will be called with a ColorScheme table
+	on_colors = function(colors)
+		colors.bg_highlight = "#292e46"
+	end,
+})
 
 -- Configure rust-analyzer.cargo.sysroot in LunaVim:
-require'lspconfig'.rust_analyzer.setup{
-    settings = {
-        ["rust-analyzer.cargo.sysroot"] = "/Users/sarath.lun/.rustup/toolchains/stable-aarch64-apple-darwin"
-    }
-}
+require("lspconfig").rust_analyzer.setup({
+	settings = {
+		["rust-analyzer.cargo.sysroot"] = "/Users/sarath.lun/.rustup/toolchains/stable-aarch64-apple-darwin",
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+	},
+})
 
 -- select color theme
-lvim.colorscheme = "tokyonight-night"
+-- lvim.colorscheme = "tokyonight"
 
 lvim.plugins = {
-  -- install colorscheme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+	-- install colorscheme
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 
-  -- install rainbow_csv
-  {'cameron-wags/rainbow_csv.nvim',
-    config = true,
-    ft = {
-        'csv',
-        'tsv',
-        'csv_semicolon',
-        'csv_whitespace',
-        'csv_pipe',
-        'rfc_csv',
-        'rfc_semicolon'
-    },
-    cmd = {
-        'RainbowDelim',
-        'RainbowDelimSimple',
-        'RainbowDelimQuoted',
-        'RainbowMultiDelim'
-    },
-  },
-  -- install markdown-preview
-  {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    ft = "markdown",
-    lazy = true,
-    config = function()
-      vim.g.mkdp_auto_start = 0
-    end,
-  },
-
+	{
+		"saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		config = function()
+			require("crates").setup()
+		end,
+	},
+	-- install rainbow_csv
+	{
+		"cameron-wags/rainbow_csv.nvim",
+		config = true,
+		ft = {
+			"csv",
+			"tsv",
+			"csv_semicolon",
+			"csv_whitespace",
+			"csv_pipe",
+			"rfc_csv",
+			"rfc_semicolon",
+		},
+		cmd = {
+			"RainbowDelim",
+			"RainbowDelimSimple",
+			"RainbowDelimQuoted",
+			"RainbowMultiDelim",
+		},
+	},
+	-- install markdown-preview
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		ft = "markdown",
+		lazy = true,
+		config = function()
+			vim.g.mkdp_auto_start = 0
+		end,
+	},
 }
-
